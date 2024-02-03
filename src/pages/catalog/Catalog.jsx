ab093b9d24e93cffbfad45f15ff6f4c9293f60d6
Nurillo04 <aminjonovnurillo95@gmail.com>
2024-02-03 17:40:21 +0500

    contact

--- src/pages/catalog/Catalog.jsx
+++ src/pages/catalog/Catalog.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Advertising from "../../comments/advertising/Advertising";
 import Cards from "../../comments/cards/Cards";
+import Contact from "../../comments/contact/Contact";
 import "./Catalog.scss";
 
 const Catalog = () => {
   return (
     <div>
       <Cards />
+      <Advertising />
+      <Contact />
     </div>
   );
 };
